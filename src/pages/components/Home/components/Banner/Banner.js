94ea3e6c8383380de75b3411e91ac7c11160a41a
Volodymyr Padovsky <olesia.dovbush.if.2015@lpnu.ua>
2019-10-15 17:08:49 +0300

    Added Banner video

--- src/pages/components/Home/components/Banner/Banner.js
+++ src/pages/components/Home/components/Banner/Banner.js
@@ -7,13 +7,24 @@ export class Banner extends Component {
             <section className="home__banner">
                 <div className="container home__banner__block">
                     <div className="wrapper">
-                        <Bubble size="351px" top="115px" left="200px" right="-255px" speed="0.8"/>
-                        <Bubble size="470px" top="55px" left="-270px" speed="0.3"/>
-                        <img src="assets/img/hero-elephant-notebook.png" alt="elephant" />
+                        {/*<Bubble size="351px" top="115px" left="200px" right="-255px" speed="0.8"/>*/}
+                        {/*<Bubble size="470px" top="55px" left="-270px" speed="0.3"/>*/}
+                        {/*<img src="assets/img/hero-elephant-notebook.png" alt="elephant" />*/}
+                        <video
+                            autoPlay
+                            muted
+                            // loop
+                            width="100%"
+                        >
+                            <source
+                                src="assets/video/oril-banner.mp4"
+                                type="video/mp4"
+                            />
+                            Sorry, your browser doesn't support embedded videos.
+                        </video>
                     </div>
                     <div className="home__banner__slogan">
-                        <h1>ORIL</h1>
-                        <h2>One stop shop <br/> for your digital product</h2>
+                        <h2>One stop shop for your digital product</h2>
                     </div>
                 </div>
             </section>
