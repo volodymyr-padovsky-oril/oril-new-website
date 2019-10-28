cfd58871d8b5c9b8652af92d3ad0aaaaeda5474b
Volodymyr Padovsky <olesia.dovbush.if.2015@lpnu.ua>
2019-10-28 18:06:07 +0200

    Added react helmet

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,6 +5,7 @@ import Footer from "../commons/Footer";
 import Contacts from "../commons/Contacts";
 import Snackbar from "../commons/SnackBar";
 import routes from "../routes";
+import { Helmet } from 'react-helmet';
 
 import "./index.scss";
 
@@ -16,6 +17,11 @@ class App extends Component {
     render() {
         return (
             <Router>
+                <Helmet>
+                    <title>ORIL</title>
+                    <meta name="description" content="ORIL is a software development company focusing on web, mobile app and IoT products, MVP for software startups and IT consulting." />
+                    <meta name="theme-color" content="#000000" />
+                </Helmet>
                 <Header redirect={this.onChangePage}/>
                     {routes()}
                 <Contacts/>
