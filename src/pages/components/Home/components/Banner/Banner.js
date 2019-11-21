dd524e0c3c3a2256b94d4b654ee4e263659cd2d8
Volodymyr Padovsky <olesia.dovbush.if.2015@lpnu.ua>
2019-11-21 12:17:05 +0200

    Fixed Banner responsive

--- src/pages/components/Home/components/Banner/Banner.js
+++ src/pages/components/Home/components/Banner/Banner.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import Bubble from "../../../../../commons/Bubble";
 import { ReactComponent as Logo } from './oril_banner.svg';
 import { ReactComponent as Title } from './oril_banner_title.svg';
 
@@ -9,11 +8,8 @@ export class Banner extends Component {
             <section className="home__banner">
                 <div className="container home__banner__block">
                     <div className="wrapper">
-                        {/*<Bubble size="351px" top="115px" left="200px" right="-255px" speed="0.8"/>*/}
-                        {/*<Bubble size="470px" top="55px" left="-270px" speed="0.3"/>*/}
                         <div id="border_wrap">
                             <Logo/>
-
                         </div>
                     </div>
                     <div className="home__banner__slogan">
