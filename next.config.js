ad83ac2136447bad3fa41d203c27fb99c7f8dc7b
Volodymyr Padovsky <olesia.dovbush.if.2015@lpnu.ua>
2019-12-23 15:50:43 +0200

    Changed config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,36 @@
-const withSass = require('@zeit/next-sass');
+const { parsed: localEnv } = require("dotenv").config();
+const webpack = require("webpack");
+const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS(withSass({
-    webpack (config, options) {
-        config.module.rules.push({
-            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-            use: {
-                loader: 'css-loader',
-                options: {
-                    limit: 100000
-                }
-            }
-        });
+
+module.exports = {
+    webpack(config) {
+        config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
         return config;
     }
-}));
+};
 
-const sitemap = require('nextjs-sitemap-generator');
-sitemap({
-    baseUrl: 'https://oril.co/',
-    pagesDirectory: __dirname + "/pages",
-    targetDirectory : 'static/'
-});
+module.exports = withSass(
+    withCSS({
+        webpack(config, options) {
+            config.module.rules.push({
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                use: {
+                    loader: "css-loader",
+                    options: {
+                        limit: 100000
+                    }
+                }
+            });
+
+            return config;
+        },
+        env: {
+            PUBLIC_URL: process.env.PUBLIC_URL,
+            REACT_APP_API_HOST: process.env.REACT_APP_API_HOST,
+            REACT_APP_SOCKET_API_HOST: process.env.REACT_APP_SOCKET_API_HOST,
+            REACT_APP_STRIPE_PUB_KEY: process.env.REACT_APP_STRIPE_PUB_KEY
+        }
+    })
+);
