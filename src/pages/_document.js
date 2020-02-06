ebdbbefe1fa36fc29c6479acf09b916d622a3f02
Volodymyr Padovsky <olesia.dovbush.if.2015@lpnu.ua>
2020-02-06 15:39:17 +0200

    Testing og

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,19 +1,26 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {Html, Head, Main, NextScript} from 'next/document'
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
+        return {...initialProps}
     }
 
     render() {
         return (
             <Html>
-                <Head />
+                <Head/>
+                <head>
+                    <meta property="og:title" content="Example title"/>
+                    <meta property="og:type" content="article"/>
+                    <meta property="og:url" content="http://www.site.com/example"/>
+                    <meta property="og:image" content="http://site.com/images/example.jpg"/>
+                </head>
                 <body>
-                <Main />
-                <NextScript />
-                <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/6728466.js"></script>
+                <Main/>
+                <NextScript/>
+                <script type="text/javascript" id="hs-script-loader" async defer
+                        src="//js.hs-scripts.com/6728466.js"></script>
                 </body>
             </Html>
         )
