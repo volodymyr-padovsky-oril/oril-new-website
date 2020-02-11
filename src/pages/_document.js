6786f69b913e2ad2b673847fd5a298394b1dc8f8
Volodymyr Padovsky <olesia.dovbush.if.2015@lpnu.ua>
2020-02-11 16:54:15 +0200

    Removed meta from document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,17 +10,11 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head/>
-                <head>
-                    <meta property="og:title" content="Example title"/>
-                    <meta property="og:type" content="article"/>
-                    <meta property="og:url" content="http://www.site.com/example"/>
-                    <meta property="og:image" content="http://site.com/images/example.jpg"/>
-                </head>
                 <body>
-                <Main/>
-                <NextScript/>
-                <script type="text/javascript" id="hs-script-loader" async defer
-                        src="//js.hs-scripts.com/6728466.js"></script>
+                    <Main/>
+                    <NextScript/>
+                    <script type="text/javascript" id="hs-script-loader" async defer
+                            src="//js.hs-scripts.com/6728466.js"></script>
                 </body>
             </Html>
         )
