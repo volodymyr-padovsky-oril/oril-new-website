b9228f4e688f8f142c23f7d67d30e0e458f639fc
Volodymyr Padovsky <olesia.dovbush.if.2015@lpnu.ua>
2020-02-12 14:26:31 +0200

    Added fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,16 @@ import '../index.css';
 import SEOMetaTags from "../components/SEOMetaTags";
 import axios from 'axios';
 
+// import fetch from 'unfetch'
+// import useSWR from 'swr'
+//
+// const API_URL = 'https://api.github.com'
+// async function fetcher(path) {
+//     const res = await fetch(API_URL + path)
+//     const json = await res.json()
+//     return json
+// }
+
 class Index extends React.Component {
 
     // static async getInitialProps(ctx) {
@@ -16,10 +26,30 @@ class Index extends React.Component {
     //     return {data: res.data}
     // }
     // componentDidMount() {
+    //     const res = await axios.get('https://api.ipdata.co/en?api-key=04b4e64842a9fd97b191d94431fcc4605964bc8c65174d5e6c052025');
+    //     return {data: res.data}
     //     console.log('data-index', this.props.data.country_name);
     // }
 
+    // static async getInitialProps() {
+    //     let res = await fetch('https://api.ipdata.co/en?api-key=04b4e64842a9fd97b191d94431fcc4605964bc8c65174d5e6c052025')
+    //     let contriesObj = await res.json();
+    //     console.log(contriesObj)
+    //     return {countries: contriesObj.country_name}
+    // }
+
+    static async getInitialProps() {
+        let res = await fetch('https://api.ipdata.co/en?api-key=04b4e64842a9fd97b191d94431fcc4605964bc8c65174d5e6c052025')
+        let contriesObj = await res.json()
+        console.log('getInitialProps', contriesObj)
+        return contriesObj
+    }
+
     render() {
+        // const { data, error } = useSWR('/repos/zeit/next.js', res)
+        // console.log('swr', this.props.json);
+        console.log('this.props.contriesObj', this.props.contriesObj)
+
         return (
             <>
                 <SEOMetaTags
@@ -32,9 +62,12 @@ class Index extends React.Component {
                         image: "https://oril.co/assets/img/link-logo.png"
                     }}
                 />
+                {/*if (error) return <div>failed to load</div>*/}
+                {/*if (!data) return <div>loading...</div>*/}
+                {/*return <div>Next stars: {data.stargazers_count}</div>*/}
                 <Header/>
                 <Home/>
-                <Contacts data={this.props.data}/>
+                <Contacts contriesObj={this.props.contriesObj}/>
                 <Footer/>
             </>
         )
