8450fdb54ae694ca962753f643a971da53906f3f
Volodymyr Padovsky <olesia.dovbush.if.2015@lpnu.ua>
2020-02-26 14:52:58 +0200

    Added apng visibility on scroll

--- src/components/ServiceCard/ServiceCard.js
+++ src/components/ServiceCard/ServiceCard.js
@@ -2,8 +2,12 @@ import React, {Component} from "react";
 import './_service-card.scss';
 import Bubble from "../../commons/Bubble";
 import Link from "next/link";
+import VizSensor from "react-visibility-sensor";
 
 class ServiceCard extends Component {
+    state = {
+        imgViz: false
+    };
 
     render() {
         const {direction} = this.props;
@@ -51,7 +55,17 @@ class ServiceCard extends Component {
                                 <Bubble size="490px" top="280px" right="110px" speed=".7"/>
                                 <Bubble size="400px" top="250px" right="20px" speed=".7"/>
                                 <Bubble size="200px" top="10px" right="-200px" speed=".7"/>
-                                <img src="assets/gif/PD-compressed.png" alt="development"/>
+                                <VizSensor
+                                    minTopValue={200}
+                                    partialVisibility={true}
+                                    onChange={(isVisible) => {
+                                        this.setState({imgViz: isVisible})
+                                    }}
+                                >
+                                    <img
+                                        src={`${this.state.imgViz ? 'assets/gif/PD-compressed.png' : 'assets/gif/transparent.png'}`}
+                                        alt="development"/>
+                                </VizSensor>
                             </div>
                         </div>
                 }
