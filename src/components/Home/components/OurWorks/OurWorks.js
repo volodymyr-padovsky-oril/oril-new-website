b6b748cde42c92684585094797647da6529fa19b
Volodymyr <volodymyr.padovsky@orilinc.com>
2020-09-01 19:57:19 +0300

    Fixed Our words responsive

--- src/components/Home/components/OurWorks/OurWorks.js
+++ src/components/Home/components/OurWorks/OurWorks.js
@@ -11,16 +11,19 @@ export const params = {
         clickable: true
     },
     slidesPerView: 3,
+    spaceBetween: 50,
     autoplay: {
         delay: 3500,
         disableOnInteraction: false
     },
-    width: 1240,
-
     breakpoints: {
-        1260: {
+        900: {
             slidesPerView: 1,
         },
+        1239: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        }
     }
 };
 
@@ -41,34 +44,36 @@ export class OurWorks extends Component {
         return (
             <section className="home__our-works">
                 <h3>{this.props.title}</h3>
-                <div className="home__our-works__wrapper">
-                    <Swiper {...params}>
-                        {this.slides.map((slide, index) => (
-                                <div key={'slide-' + index}>
-                                    <div className="slide">
-                                        <img src={`../assets/img/${slide.img.name}${slide.img.extension}`}
-                                             alt={slide.img.name}/>
-                                        <div>
-                                            <div
-                                                className="title">{slide.title}
+                <div className="container">
+                    <div className="home__our-works__wrapper">
+                        <Swiper {...params}>
+                            {this.slides.map((slide, index) => (
+                                    <div key={'slide-' + index}>
+                                        <div className="slide">
+                                            <img src={`../assets/img/${slide.img.name}${slide.img.extension}`}
+                                                 alt={slide.img.name}/>
+                                            <div>
+                                                <div
+                                                    className="title">{slide.title}
+                                                </div>
+                                                <div className="tags">
+                                                    {
+                                                        slide.tags.map((tag, index) => (
+                                                            <span key={`tag-${index}`}>{tag}</span>))
+                                                    }
+                                                </div>
+                                                <p className="text">{slide.text}</p>
+                                                <Link href={`case-study/${slide.link}`}>
+                                                    <a className="link">Learn more
+                                                    </a>
+                                                </Link>
                                             </div>
-                                            <div className="tags">
-                                                {
-                                                    slide.tags.map((tag, index) => (
-                                                        <span key={`tag-${index}`}>{tag}</span>))
-                                                }
-                                            </div>
-                                            <p className="text">{slide.text}</p>
-                                            <Link href={`case-study/${slide.link}`}>
-                                                <a className="link">Learn more
-                                                </a>
-                                            </Link>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        )}
-                    </Swiper>
+                                )
+                            )}
+                        </Swiper>
+                    </div>
                 </div>
             </section>
         );
