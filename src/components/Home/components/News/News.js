518050b20afca703d208b5486c51b5f60ec7f609
Volodymyr <volodymyr.padovsky@orilinc.com>
2020-09-07 19:04:33 +0300

    updates

--- src/components/Home/components/News/News.js
+++ src/components/Home/components/News/News.js
@@ -1,6 +1,21 @@
 import React, {Component} from "react";
 import {news} from "../../../../lib/news";
+import Swiper from "react-id-swiper";
 
+export const params = {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    slidesPerView: 1,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false
+    },
+    speed: 1500
+};
 
 class OurNews extends Component {
     render() {
@@ -25,6 +40,25 @@ class OurNews extends Component {
                             </div>
                         )
                     })}
+                </div>
+                <div className="home__our-news__wrapper--mobile">
+                    <Swiper {...params}>
+                        {news.map((item, index) => {
+                            return (
+                                <div className="home__our-news__card">
+                                    {/*<img className="first-img" src={`/assets/img/${item.img}.png`} alt={item.img}/>*/}
+                                    <img className="second-img" src={`/assets/img/${item.img}-darken.png`}
+                                         alt={item.img}/>
+                                    <div className="home__our-news__cloud">{item.title}
+                                        <a href={item.href}>
+                                            Read more
+                                        </a>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </Swiper>
+
                 </div>
             </section>
         );
