9c2f1f8d6395c4de1c1c65f5fd20747bc4ed71f1
Oril <oleh.sliusarenko@gmail.com>
2020-09-11 10:24:46 +0200

    Added fade out animation for all workers

--- src/components/Home/components/WeAre/WeAreItem.js
+++ src/components/Home/components/WeAre/WeAreItem.js
@@ -8,16 +8,17 @@ export class WeAreItem extends Component {
       visible: false,
     };
   }
-  
+
   componentWillReceiveProps(props, state) {
-    if(props.visible !== state.visible) {
-      props.visible &&  this.setState({ hover: false });
+    if (props.visible !== state.visible) {
+      props.visible && this.setState({ hover: false });
     }
   }
 
   render() {
+    console.log();
     const { worker, index } = this.props;
-    const { hover } = this.state
+    const { hover } = this.state;
     const animal_src = "assets/img/" + worker.animal + ".png";
     const photoStyles = {
       backgroundImage: "url(assets/img/" + worker.photo + ".jpg)",
@@ -26,54 +27,41 @@ export class WeAreItem extends Component {
     };
 
     const cardFront = {
-      opacity: 1,
+      opacity: 0,
       transform: "scale(.8)",
       border: "1px solid red",
-      transitionDelay: `${1 + (index * .5)}s `,
     };
 
     const cardBack = {
       opacity: 1,
       transform: "scale(1)",
+      transitionDelay: ".2s",
       backgroundImage: "url(assets/img/" + worker.photo + ".jpg)",
       backgroundSize: "cover",
       backgroundRepeat: "no-repeat",
     };
-
-    const cardBack_1 = {
-      // opacity: 1,
-      // transform: "scale(1)",
-      transitionDelay: `${1 + (index * .3)}s `,
-      backgroundImage: "url(assets/img/" + worker.photo + ".jpg)",
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-    }
 
     const description = {
       opacity: 1,
       transform: "scale(1)",
       transitionDelay: ".2s",
     };
-    
-    const description_1 = {
-      transitionDelay: `${1 + (index * .3)}s `,
-    }
 
     return (
       <div className="card" key={"card-" + index}>
         <div
           className="card__front"
-          // style={worker.hover ? (hover ? null : cardFront) : null}
+          style={worker.hover && hover ? cardFront : null}
         >
           <img src={animal_src} alt={worker.animal} />
         </div>
         <div
           className="card__back"
-          style={worker.hover ? (hover ? cardBack : cardBack_1) : photoStyles}
+          style={worker.hover && hover ? cardBack : photoStyles}
         >
           <div
             className="card__back__desc"
-            style={worker.hover ? (hover ? description : description_1) : null}
+            style={worker.hover && hover ? description : null}
           >
             <span>{worker.name}</span>
             <span>{worker.position}</span>
