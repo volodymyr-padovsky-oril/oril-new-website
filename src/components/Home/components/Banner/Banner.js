e3ffe0540285b5810867480e9771d3fb13dddd53
Volodymyr <volodymyr.padovsky@orilinc.com>
2020-09-16 11:53:12 +0300

    Added new Banner

--- src/components/Home/components/Banner/Banner.js
+++ src/components/Home/components/Banner/Banner.js
@@ -5,17 +5,18 @@ export class Banner extends Component {
         return (
             <section className="banner">
                 <div className="banner__block">
+                    <h1>One stop shop for <br/> your digital product</h1>
+                    <img src="../assets/img/top-wave.png" alt="top-wave"/>
                 </div>
-                <div className="container">
-
-                    <div className="banner__text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ea
-                            earum fugiat neque odio porro praesentium quod repellendus sapiente! Ad amet aspernatur
-                            assumenda
-                            labore mollitia quis quod reiciendis sequi voluptas!
-                        </p>
-                    </div>
-                </div>
+                {/*<div className="container">*/}
+                {/*    <div className="banner__text">*/}
+                {/*        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ea*/}
+                {/*            earum fugiat neque odio porro praesentium quod repellendus sapiente! Ad amet aspernatur*/}
+                {/*            assumenda*/}
+                {/*            labore mollitia quis quod reiciendis sequi voluptas!*/}
+                {/*        </p>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </section>
         );
     }
