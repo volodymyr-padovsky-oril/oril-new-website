84fe108f4e6c1bd624a35e40adaeda27ae23f842
Oril <oleh.sliusarenko@gmail.com>
2020-09-29 17:15:45 +0200

    Fixed bugs

--- src/components/ImageLoader/index.js
+++ src/components/ImageLoader/index.js
@@ -3,15 +3,15 @@ import React from "react";
 
 class ProgressiveImage extends React.Component {
 
-    static defaultProps = {
+    state = {
         transitionTime: 500,
         timingFunction: "ease",
-        initialBlur: 10
+        initialBlur: 10,
     };
 
     unmounted = false;
 
-    componentWillMount() {
+    componentDidMount() {
         const {src, preview} = this.props;
         const initialBlur = this.props.initialBlur;
         this.setState({ src: preview, blur: initialBlur });
@@ -27,12 +27,14 @@ class ProgressiveImage extends React.Component {
     render() {
         const {src} = this.state;
         const {render} = this.props;
-        return render(src, this.getStyle());
+        return (
+            render(src, this.getStyle()))
     }
 
-    fetch(src) {
+    fetch(src = "") {
         return new Promise(resolve => {
             const image = new Image();
+            console.log(src)
             image.src = src;
             image.addEventListener("load", () => resolve(src), false);
         });
