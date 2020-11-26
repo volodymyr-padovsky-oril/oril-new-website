45656830f8fb8587600858d9d363f56c130b366d
Volodymyr <volodymyr.padovsky@orilinc.com>
2020-11-26 11:21:55 +0200

    fixed video play

--- src/components/Home/components/Banner/Banner.js
+++ src/components/Home/components/Banner/Banner.js
@@ -1,31 +1,28 @@
-import React, { Component } from "react";
-import Header from "../../../../commons/Header";
+import React, {Component} from "react";
 import Delayed from "../../../../commons/utils/Delayed";
 
 export class Banner extends Component {
     render() {
         return (
-            /*<div className="banner-wrapper">*/
             <section className="banner">
                 <div className="banner__block">
-                    {/*<img className="banner-img--desktop" src="../assets/img/top-wave__plug.png" alt="top-wave__plug"/>*/}
-                    {/*<img className="banner-img--desktop__lazy" src="../assets/img/top-wave__lazy.png" alt="top-wave"/>*/}
-                    {/* <img className="banner-img--desktop__lazy" src="../assets/img/top-wave.png" alt="top-wave"/> */}
-
-                    {/*<img className="banner-img--mobile" src="../assets/img/top-wave-mobile.png" alt="top-wave"/>*/}
-                    <video className="banner-img--desktop__lazy" loop autoPlay muted playsinline>
-                        <source src="../assets/video/main-banner.mp4" type="video/mp4" />
-                        {/* <source src="movie.ogg" type="video/ogg"/> */}
+                    <video className="banner-img--desktop" loop autoPlay muted playInline>
+                        <source src="../assets/video/main-banner.mp4" type="video/mp4"/>
+                        Your browser does not support the video tag.
+                    </video>
+                    <video className="banner-img--mobile" muted="" loop autoplay="" kind="captions" preload="auto"
+                           playsinline="">
+                        autoPlay="" kind="captions" preload="auto" playsinline="">
+                        <source src="../assets/video/main-banner.mp4" type="video/mp4"/>
                         Your browser does not support the video tag.
                     </video>
                     <div className="header-wrapper">
                         <Delayed waitBeforeShow={500}>
-                            <h1>One stop shop <br /> for your <br /> digital product</h1>
+                            <h1>One stop shop <br/> for your <br/> digital product</h1>
                         </Delayed>
                     </div>
                 </div>
             </section>
-            // </div>
         );
     }
 }
